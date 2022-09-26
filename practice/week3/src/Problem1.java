4d99e11b40e48352ee166cd8fe6a8423ea34853a
TuanDao2002 <s3877347@rmit.edu.vn>
2022-09-26 12:32:32 +0700

    update

---
+++ practice/week3/src/Problem1.java
@@ -0,0 +1,9 @@
+public class Problem1 {
+    static int[] TwoDaysWithMaxInc(int[] arr) {
+        int[] pairOfDays = new int[2];
+        return pairOfDays;
+    }
+    public static void main(String[] args) {
+
+    }
+}
