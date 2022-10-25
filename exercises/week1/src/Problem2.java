ee1068537eea203ce1d0f0305f40184e864b6821
TuanDao2002 <s3877347@rmit.edu.vn>
2022-10-25 17:13:30 +0700

    update

---
+++ exercises/week1/src/Problem2.java
@@ -0,0 +1,28 @@
+public class Problem2 {
+    static int findMissingNumber(int[] arr) {
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        int sumOfArr = 0;
+
+        for (int val : arr) {
+            if (val > max) {
+                max = val;
+            }
+
+            if (val < min) {
+                min = val;
+            }
+
+            sumOfArr += val;
+        }
+
+        int N = arr.length;
+        int sum = N * (N + 1) / 2;
+        return sum - sumOfArr;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{0, 1, 2, 4, 5};
+        System.out.println(findMissingNumber(arr));
+    }
+}
---
+++ exercises/week1/src/Problem3.java
@@ -0,0 +1,19 @@
+public class Problem3 {
+    static boolean isPermutation(int[] arr1, int[] arr2) {
+        int len = arr1.length;
+
+        for (int i = 0; i < len; i++) {
+
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = new int[]{1, 2, 3, 4};
+        int[] arr2 = new int[]{0, 3, 3, 4};
+        if (isPermutation(arr1, arr2)) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
