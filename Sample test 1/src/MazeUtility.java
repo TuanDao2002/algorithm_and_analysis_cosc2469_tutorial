0e4b058185b493a47e3b168b42beb418df418921
TuanDao2002 <s3877347@rmit.edu.vn>
2022-11-18 14:26:13 +0700

    update

--- Sample test 1/src/MazeUtility.java
+++ Sample test 1/src/MazeUtility.java
@@ -52,8 +52,8 @@ public class MazeUtility {
     public static void main(String[] args) {
         char[][] maze = new char[][]{
                 {'S', '.', '.', '*'},
-                {'.', '*', '*', '*'},
-                {'.','.', '.', 'E'}
+                {'*', '*', '.', '.'},
+                {'.', '*', '*', 'E'}
         };
 
         displayMaze(maze);
